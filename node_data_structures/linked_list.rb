e4301405d4a647edf60fb7b46f02e4aaed2efb46
Cameron Tong <cameron.tong2b@gmail.com>
2021-12-30 17:40:02 -0500

    finish linked list coverage

--- node_data_structures/linked_list.rb
+++ node_data_structures/linked_list.rb
@@ -14,12 +14,70 @@ class LinkedList
     @head = head
   end
 
-  def add(value)
+  def unshift(value)
     new_node = Node.new(value)
     new_node.next = head
     @head = new_node
   end
 
+  def insert_at_index(index, value)
+    new_node = Node.new(value)
+    current_node = head
+    current_index = 0
+
+    return unshift(value) if index == 0
+
+    until current_index == index - 1
+      current_index += 1
+      current_node = current_node.next
+    end
+
+    temp = current_node.next
+    current_node.next = new_node
+    new_node.next = temp
+    new_node
+  end
+
   def read(index)
+    current_index = 0
+    current_node = head
+
+    until current_index == index
+      current_index += 1
+      current_node = current_node.next
+      return nil unless current_node
+    end
+
+    current_node.data
+  end
+
+  def index_of(value)
+    current_index = 0
+    current_node = head
+
+    while current_node.data != value
+      current_node = current_node.next
+      current_index += 1
+      return nil unless current_node
+    end
+
+    current_index
+  end
+
+  def delete_at_index(index)
+    current_node = head
+    current_index = 0
+
+    if index == 0
+      @head = @head.next
+      return
+    end
+
+    until current_index == index - 1
+      current_node = current_node.next
+      current_index += 1
+      return nil unless current_node
+    end
+    current_node.next = current_node.next.next
   end
 end
--- node_data_structures/linked_list_spec.rb
+++ node_data_structures/linked_list_spec.rb
@@ -18,30 +18,116 @@ RSpec.describe Node do
 end
 
 RSpec.describe LinkedList do
-  it "takes a node element as the head" do
-    head_node = Node.new(nil)
-    actual = LinkedList.new(head_node)
+  context "#initialize" do
+    it "takes a node element as the head" do
+      head_node = Node.new(nil)
+      actual = LinkedList.new(head_node)
 
-    expect(actual.head).to eq(head_node)
+      expect(actual.head).to eq(head_node)
+    end
+
+    it "can unshift to list by reassigning head" do
+      head_node = Node.new(1)
+      list = LinkedList.new(head_node)
+
+      list.unshift(3)
+
+      expect(list.head.data).to be(3)
+      expect(list.head.next.data).to be(1)
+    end
+  end
+  context "#read" do
+    it "reads to a given index" do
+      node = Node.new(1)
+      list = LinkedList.new(node)
+      list.unshift(4)
+      list.unshift(5)
+
+      actual = list.read(2)
+
+      expect(actual).to eq(1)
+    end
+
+    it "returns nil if index does not exist" do
+      node = Node.new(1)
+      list = LinkedList.new(node)
+      list.unshift(2)
+      list.unshift(3)
+
+      actual = list.read(4)
+
+      expect(actual).to be nil
+    end
   end
 
-  it "can add to list by reassigning head" do
-    head_node = Node.new(1)
-    list = LinkedList.new(head_node)
+  context "#index_of" do
+    it "returns the index if the search value" do
+      node = Node.new(7)
+      list = LinkedList.new(node)
+      list.unshift(8)
+      list.unshift(9)
 
-    list.add(3)
+      actual = list.index_of(8)
 
-    expect(list.head.data).to be(3)
-    expect(list.head.next.data).to be(1)
+      expect(actual).to eq(1)
+    end
+
+    it "returns nil if the value is not found" do
+      node = Node.new(7)
+      list = LinkedList.new(node)
+      list.unshift(8)
+      list.unshift(9)
+
+      actual = list.index_of(10)
+
+      expect(actual).to be nil
+    end
   end
 
-  xit "reads to a given index" do
-    list = LinkedList.new(node)
-    list.add(4)
-    list.add(5)
+  context "#insert_at_index" do
+    it "inserts at any given index" do
+      node = Node.new(6)
+      list = LinkedList.new(node)
+      list.unshift(8)
+      list.unshift(9)
 
-    actual = list.read(2)
+      list.insert_at_index(2, 7)
 
-    expect(actual).to eq(5)
+      expect(list.read(2)).to eq(7)
+    end
+
+    it "can reassign the head" do
+      node = Node.new(6)
+      list = LinkedList.new(node)
+      list.unshift(8)
+      list.unshift(9)
+
+      list.insert_at_index(0, 7)
+
+      expect(list.head.data).to eq(7)
+    end
+  end
+
+  context "#delte_at_index" do
+    it "deletes in the middle of the list" do
+      node = Node.new(6)
+      list = LinkedList.new(node)
+      list.unshift(8)
+      list.unshift(9)
+
+      list.delete_at_index(1)
+
+      expect(list.read(1)).to eq(6)
+    end
+    it "deletes the head" do
+      node = Node.new(6)
+      list = LinkedList.new(node)
+      list.unshift(8)
+      list.unshift(9)
+
+      list.delete_at_index(0)
+
+      expect(list.head.data).to eq(8)
+    end
   end
 end
